2afd7c9faa87425e4abd268f9c093639f68c387d
YOGEESH K P <yogeesh793@gmail.com>
2025-02-27 10:01:39 +0530

    Add files via upload

---
+++ 6.NumPy.py
@@ -0,0 +1,73 @@
+# Create an array with 5 dimensions and verify that it has 5 dimension
+import numpy as np
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print(arr)
+print('Number of dimensions :', arr.ndim)
+
+#2
+import numpy as yo
+kp=yo.array([5,6,4,7],ndmin=5)
+print(kp)
+print('Number of dimensions :', kp.ndim)
+
+#3
+import numpy as bp
+brr=bp.array([1,2,3,4],ndmin=5)
+print(brr)
+print('Number of Dimention:',brr.ndim)
+
+#4
+import numpy as cp
+crr=cp.array([5,6,7,8],ndmin=5)
+print(crr)
+print('number of dimention:',crr.ndim)
+
+#5
+import numpy as dp
+drr=dp.array([1,2,3,4],ndmin=5)
+print(drr)
+print('number of dimention:',drr.ndim)
+
+#6
+import numpy as ep
+err=ep.array([1,2],ndmin=2)
+print(err)
+print('number of dimention:',err.ndim)
+
+#7
+import numpy as fp
+frr=fp.array([1,2,3],ndmin=3)
+print(frr)
+print('number of dimention:',frr.ndim)
+
+#8
+import numpy as gp
+grr = gp.array([1],ndmin=1)
+print(grr)
+print('number of dimention:',grr.ndim)
+
+#9
+import numpy as hp
+hrr=hp.array([1,2,3,4],ndmin=4)
+print(hrr)
+print('number of dimention:',hrr.ndim)
+
+#10
+import numpy as ip
+irr=ip.array([1,2,3,4,5],ndmin=6)
+print(irr)
+print('number of dimention:',irr.ndim)
+
+#11
+import numpy as jp
+jrr=jp.array([1,2,3],ndmin=4)
+print(jrr)
+print('number of dimention:',jrr.ndim)
+
+#12
+import numpy as kp
+krr=kp.array([1,2,3,4,5,6,7,8,9],ndmin=10)
+print(krr)
+print('number of dimention:',krr.ndim)
+
+
